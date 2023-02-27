4e1a206c541b2b4b60c9a457e9487acd507079f7
José Ángel Marín <joseangelmarin@gmail.com>
2023-02-27 17:04:38 +0100

    Initial Commit

---
+++ cursoSwiftUI/cursoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  cursoSwiftUIApp.swift
+//  cursoSwiftUI
+//
+//  Created by Jose on 27/2/23.
+//
+
+import SwiftUI
+
+@main
+struct cursoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
